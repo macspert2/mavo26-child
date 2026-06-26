9027d318efb9ca1a00eece8fef0654e8f3b5b6e7
macspert2 <macspert2@icloud.com>
2026-06-26 13:21:56 +0100

    editor

--- functions.php
+++ functions.php
@@ -15,6 +15,62 @@ remove_image_size('1536x1536');
 remove_image_size('2048x2048');
 
 add_editor_style( 'editor-style.css' );
+function mytheme_change_tinymce_colors( $init ) {
+    $default_colours = '
+        "000000", "Black",
+        "993300", "Burnt orange",
+        "333300", "Dark olive",
+        "003300", "Dark green",
+        "003366", "Dark azure",
+        "000080", "Navy Blue",
+        "333399", "Indigo",
+        "333333", "Very dark gray",
+        "800000", "Maroon",
+        "FF6600", "Orange",
+        "808000", "Olive",
+        "008000", "Green",
+        "008080", "Teal",
+        "0000FF", "Blue",
+        "666699", "Grayish blue",
+        "808080", "Gray",
+        "FF0000", "Red",
+        "FF9900", "Amber",
+        "99CC00", "Yellow green",
+        "339966", "Sea green",
+        "33CCCC", "Turquoise",
+        "3366FF", "Royal blue",
+        "800080", "Purple",
+        "999999", "Medium gray",
+        "FF00FF", "Magenta",
+        "FFCC00", "Gold",
+        "FFFF00", "Yellow",
+        "00FF00", "Lime",
+        "00FFFF", "Aqua",
+        "00CCFF", "Sky blue",
+        "993366", "Brown",
+        "C0C0C0", "Silver",
+        "FF99CC", "Pink",
+        "FFCC99", "Peach",
+        "FFFF99", "Light yellow",
+        "CCFFCC", "Pale green",
+        "CCFFFF", "Pale cyan",
+        "99CCFF", "Light sky blue",
+        "CC99FF", "Plum",
+        "FFFFFF", "White"
+        ';
+    $custom_colours = '
+        "4e74a5", "link blue",
+        "886353", "title brown",
+        "5c3d2e", "darker brown",
+        "a92d87", "purple highlight",
+        "757575", "light grey"
+        ';
+    $init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+    $init['textcolor_rows'] = 6; // expand colour grid to 6 rows
+    return $init;
+}
+add_filter('tiny_mce_before_init', 'mytheme_change_tinymce_colors');
+
 
 /**
  * Hidden homepage prototype: component CSS, only on that page template.
