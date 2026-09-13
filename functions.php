edbdb0568511507d372f6834830cc15a45921d0d
macspert2 <macspert2@icloud.com>
2026-09-13 17:46:41 +0100

    -cards

--- functions.php
+++ functions.php
@@ -810,7 +810,7 @@ if (
         $bpul
     );
 }
-        wp_enqueue_script( 'bpul_script', get_stylesheet_directory_uri() . '/js/bpul4.js', [], '1.0', true );
+        wp_enqueue_script( 'bpul_script', get_stylesheet_directory_uri() . '/js/bpulv5.js', [], '1.0', true );
         $script = 'const BPU_URL = atob("' . base64_encode( $bpul).'"); ';
         wp_add_inline_script( 'bpul_script', $script, 'before' );
     }
